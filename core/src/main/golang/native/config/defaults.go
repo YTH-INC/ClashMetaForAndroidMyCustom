cbf3ae439cf670c6639add55f630541288eda5c2
yutianhui <55071570+yutianhui@users.noreply.github.com>
2024-08-17 12:03:04 +0000

    修改默认内置dns，减少不用的dns

--- core/src/main/golang/native/config/defaults.go
+++ core/src/main/golang/native/config/defaults.go
@@ -2,10 +2,8 @@ package config
 
 var (
 	defaultNameServers = []string{
-		"223.5.5.5",
-		"119.29.29.29",
-		"8.8.4.4",
-		"1.0.0.1",
+		"https://223.5.5.5/dns-query",
+		"tcp://30.30.30.30",
 	}
 	defaultFakeIPFilter = []string{
 		// Stun Services
