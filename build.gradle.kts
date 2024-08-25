50c3e5c4dbdf99cdbfbe8fe13e9e87d83ea454af
GitHub Action <action@github.com>
2024-08-25 04:22:14 +0000

    Bump version to 2.10.4.mod-dns (210004)

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ subprojects {
             minSdk = 21
             targetSdk = 31
 
-            versionName = "2.10.3.mod-dns"
-            versionCode = 210003
+            versionName = "2.10.4.mod-dns"
+            versionCode = 210004
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
