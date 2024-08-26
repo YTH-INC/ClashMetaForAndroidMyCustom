e1500337d71f4d4baadf2e1a78242df5a7b0b023
yutianhui <55071570+yutianhui@users.noreply.github.com>
2024-08-27 00:55:51 +0800

    Update defaults.go

--- core/src/main/golang/native/config/defaults.go
+++ core/src/main/golang/native/config/defaults.go
@@ -3,7 +3,7 @@ package config
 var (
 	defaultNameServers = []string{
 		"https://223.5.5.5/dns-query#h3=true",
-		"https://1.12.12.12/dns-query",
+		"https://1.12.12.12/dns-query#h3=true",
 		"tcp://30.30.30.30",
 	}
 	defaultFakeIPFilter = []string{
